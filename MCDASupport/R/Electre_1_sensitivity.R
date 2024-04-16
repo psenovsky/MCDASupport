ed9c5e5024a0fe9c18d800ce53e277f35abd4605
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-04-16 20:10:04 +0200

    optimization of Electre_1_sensitivity function

--- MCDASupport/R/Electre_1_sensitivity.R
+++ MCDASupport/R/Electre_1_sensitivity.R
@@ -1,4 +1,5 @@
-# Sensitivity analysis of the Electre 1 function. Sensitivity test focuses on Concordance and Discordance thresholds.
+# Sensitivity analysis of the Electre 1 function. Sensitivity test
+# focuses on Concordance and Discordance thresholds.
 #
 # parameters
 #   PM - performance matrix criteria in columns and alternatives in rows
@@ -6,32 +7,34 @@
 #   concordance_threshold = c(from, to, step, default)
 #   discordance_threshold = c(from, to, step, default)
 Electre_1_sensitivity <- function(PM, w,
-                                  minmaxcriteria = 'max',
+                                  minmaxcriteria = "max",
                                   concordance_threshold,
                                   discordance_threshold) {
-
   # validate parameters
-  if (!(is.vector(concordance_threshold)) || !(is.vector(discordance_threshold))) stop('thresholds muset be set as vectors for the sensitivity analysis should be a vector')
-  if (length(concordance_threshold) != length(discordance_threshold) || length(discordance_threshold) != 4) stop('thresholds need to have 4 elements')
-  if (!is.numeric(concordance_threshold) || !is.numeric(discordance_threshold)) stop('threshold parameters need to be numbers')
-  if (concordance_threshold[1] > concordance_threshold[2] || discordance_threshold[1] > discordance_threshold[2]) stop('thresholds: from must be < then to')
-  if (concordance_threshold[1] == concordance_threshold[2] || discordance_threshold[1] == discordance_threshold[2]) stop('thresholds: from = to, no possible changes to test')
+  if (!(is.vector(concordance_threshold, mode = "numeric")) || !(is.vector(discordance_threshold, mode = "numeric"))) { # nolint: line_length_linter.
+    stop("thresholds expect numeric vectors only")
+  }
+  if (length(concordance_threshold) != 4 || length(discordance_threshold) != 4) { # nolint: line_length_linter.
+    stop("thresholds need to have 4 elements")
+  }
+  if (concordance_threshold[1] >= concordance_threshold[2] || discordance_threshold[1] >= discordance_threshold[2]) { # nolint: line_length_linter.
+    stop("thresholds: from must be < then to")
+  }
 
   alt <- rownames(PM)
-  nalt <- length(alt)
 
   # internal function to prepare row of the dataframe
-  df_row <- function(kernel, alt, sensitivity, val){
+  df_row <- function(kernel, alt, sensitivity, val) {
     nalt <- length(alt)
     t <- rep(0, times = nalt + 2)
-    for(k in kernel){
-      if(k %in% alt){
+    for (k in kernel) {
+      if (k %in% alt) {
         pos <- which(alt == k)
         t[pos] <- 1
       }
     }
-    t[nalt+1] <- sensitivity
-    t[nalt+2] <- val
+    t[nalt + 1] <- sensitivity
+    t[nalt + 2] <- val
     return(t)
   }
 
@@ -42,19 +45,17 @@ Electre_1_sensitivity <- function(PM, w,
   df <- data.frame()
 
   # sensitivity of concordance threshold, c_t must be > d_def
-  for(ct in c_t){
-    if(ct > d_def){
-      t <- Electre_1(PM = PM, w = w, minmaxcriteria = minmaxcriteria, concordance_threshold = ct, discordance_threshold = d_def, VERBOSE = FALSE)
-      df <- rbind(df, df_row(t$Kernel, alt, 'C', ct))
-    }
+  c_t2 <- c_t[c_t > d_def]
+  for (ct in c_t2) {
+    t <- Electre_1(PM = PM, w = w, minmaxcriteria = minmaxcriteria, concordance_threshold = ct, discordance_threshold = d_def, VERBOSE = FALSE)
+    df <- rbind(df, df_row(t$Kernel, alt, "C", ct))
   }
   # discordance threshold sensitivity, dt must be < c_def
-  for(dt in d_t){
-    if(dt < c_def){
-      t <- Electre_1(PM = PM, w = w, minmaxcriteria = minmaxcriteria, concordance_threshold = c_def, discordance_threshold = dt, VERBOSE = FALSE)
-      df <- rbind(df, df_row(t$Kernel, alt, 'D', dt))
-    }
+  d_t2 <- c_t[d_t < c_def]
+  for (dt in d_t2) {
+    t <- Electre_1(PM = PM, w = w, minmaxcriteria = minmaxcriteria, concordance_threshold = c_def, discordance_threshold = dt, VERBOSE = FALSE)
+    df <- rbind(df, df_row(t$Kernel, alt, "D", dt))
   }
-  colnames(df) <- c(alt, 'sensitivity', 'treshold value')
+  colnames(df) <- c(alt, "sensitivity", "treshold value")
   return(df)
 }
