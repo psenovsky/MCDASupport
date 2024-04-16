757eebfb40d72327997eddd079b23e87a9f424c8
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-04-16 07:59:42 +0200

    removal of original norm_* functions

--- MCDASupport/R/norm_zscore.R
+++
@@ -1,8 +0,0 @@
-norm_zscore <- function(tonorm){
-  #validate input
-  if(!is.vector(tonorm)) stop('tonorm paramerer expected to be vector.')
-  if(!is.numeric(tonorm)) stop('tonorm expected to be numerix vector.')
-
-  z <- (tonorm - mean(tonorm))/sd(tonorm)
-  return(z)
-}
