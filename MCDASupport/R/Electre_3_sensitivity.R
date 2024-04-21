28bdb3f261dcae0c92385b62a2e87e60d295681f
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-04-21 14:21:53 +0200

    ELECTRE III method optimizations

--- MCDASupport/R/Electre_3_sensitivity.R
+++ MCDASupport/R/Electre_3_sensitivity.R
@@ -1,3 +1,5 @@
+# Function to run sensitivity tests for Electre III method
+#
 # parameters
 #   PM - performance matrix criteria in columns and alternatives in rows
 #   w  - weights
@@ -6,13 +8,17 @@
 #   V  - veto threshold
 #   alpha - first discrimination threshold
 #   beta  - second discrimination threshold
-#   VERBOSE - in verbose mode the function prints out higher amount of information on computation
+#   VERBOSE - in verbose mode the function prints out higher amount of
+#             information on computation
 #
-# solution for the problem is dominance matrix which can be visualized into network graph
-Electre_3_sensitivity <- function(PM, w, P, Q, V, minmaxcriteria = 'max',
-                      alpha = 0.3, beta = 0.15, VERBOSE = FALSE){
+# solution for the problem is dominance matrix which can be visualized
+# into network graph
+Electre_3_sensitivity <- function(PM, w, P, Q, V, minmaxcriteria = "max",
+                                  alpha = 0.3, beta = 0.15, VERBOSE = FALSE) {
 
-  Electre_3_paramCheck(PM = PM, w = w, P = P, Q = Q, V = V, alpha = alpha, beta = beta) # common consistency check (common with Electre_3_sensitivity function)
+  # common consistency check (common with Electre_3_sensitivity function)
+  Electre_3_paramCheck(PM = PM, w = w, P = P, Q = Q, V = V, alpha = alpha,
+                       beta = beta)
   ncri <- length(P)
   nalt <- nrow(PM)
   cri <- colnames(PM)
@@ -28,18 +34,18 @@ Electre_3_sensitivity <- function(PM, w, P, Q, V, minmaxcriteria = 'max',
   hyp_P <- hyp_Q <- hyp_V <- matrix(0, nrow = 21, ncol = ncri)
   colnames(hyp_P) <- colnames(hyp_Q) <- colnames(hyp_V) <- cri
   nhyp <- 21 # value of thresholds +/- 10 % should always be 21 elements
-  for(i in 1:ncri){ # for all criteria
+  for (i in 1:ncri) { # for all criteria
     t_p <- tenPercent(P[i])
     t_q <- tenPercent(Q[i])
     t_v <- tenPercent(V[i])
-    hyp_P[,i] <- t_p
-    hyp_Q[,i] <- t_q
-    hyp_V[,i] <- t_v
+    hyp_P[, i] <- t_p
+    hyp_Q[, i] <- t_q
+    hyp_V[, i] <- t_v
 
     # sensitivity of the preference thresholds
     hyp_P_result <- hyp_Q_result <- hyp_V_result <- matrix(0, ncol = nalt, nrow = 21)
     colnames(hyp_P_result) <- colnames(hyp_Q_result) <- colnames(hyp_V_result) <- alt
-    for(j in 1:nhyp){ #iterate over values t_* for the criterium
+    for (j in 1:nhyp) { #iterate over values t_* for the criterium
       thres <- P
       thres[i] <- t_p[j]
       tp <- Electre_3(PM, w, thres, Q, V, minmaxcriteria, alpha, beta, FALSE)
@@ -49,9 +55,9 @@ Electre_3_sensitivity <- function(PM, w, P, Q, V, minmaxcriteria = 'max',
       thres <- V
       thres[i] <- t_v[j]
       tv <- Electre_3(PM, w, P, Q, thres, minmaxcriteria, alpha, beta, FALSE)
-      hyp_P_result[j,] <- tp$finalRankingUnsorted
-      hyp_Q_result[j,] <- tq$finalRankingUnsorted
-      hyp_V_result[j,] <- tv$finalRankingUnsorted
+      hyp_P_result[j, ] <- tp$finalRankingUnsorted
+      hyp_Q_result[j, ] <- tq$finalRankingUnsorted
+      hyp_V_result[j, ] <- tv$finalRankingUnsorted
     }
 
     hyp_P_cri[[i]] <- hyp_P_result
@@ -62,11 +68,11 @@ Electre_3_sensitivity <- function(PM, w, P, Q, V, minmaxcriteria = 'max',
     hyp_Q_result <- as.data.frame(hyp_Q_result)
     hyp_V_result <- as.data.frame(hyp_V_result)
 
-    title <- paste0('preference threshold for ', cri[i])
+    title <- paste0("preference threshold for ", cri[i])
     hyp_P_cri_graph[[i]] <- rankGraph(hyp_P_result, t_p, title)
-    title <- paste0('indifference threshold for ', cri[i])
+    title <- paste0("indifference threshold for ", cri[i])
     hyp_Q_cri_graph[[i]] <- rankGraph(hyp_Q_result, t_q, title)
-    title <- paste0('veto threshold for ', cri[i])
+    title <- paste0("veto threshold for ", cri[i])
     hyp_Q_cri_graph[[i]] <- rankGraph(hyp_Q_result, t_p, title)
   }
 
