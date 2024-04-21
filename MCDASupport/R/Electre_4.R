544371a560068499ac3bba188511746d56164703
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-04-21 15:11:46 +0200

    ELECTRE_4* functions optimization

--- MCDASupport/R/Electre_4.R
+++ MCDASupport/R/Electre_4.R
@@ -1,3 +1,5 @@
+# COmpare alternatives with criteria using ELECTRE IV method
+
 # parameters
 #   PM - performance matrix criteria in columns and alternatives in rows
 #   P  - preference threshold
@@ -5,11 +7,13 @@
 #   V  - veto threshold
 #   VERBOSE - TRUE to send function outputs to console
 #
-# solution for the problem is dominance matrix which can be visualized into network graph
-Electre_4 <- function(PM, P, Q, V, minmaxcriteria = 'max', VERBOSE = FALSE){
+# solution for the problem is dominance matrix which can be visualized
+# into network graph
+Electre_4 <- function(PM, P, Q, V, minmaxcriteria = "max", VERBOSE = FALSE) {
 
   # check validity of the objects manipulated by the current function
-  Electre_4_paramCheck(PM = PM, P = P, Q = Q, V = V, minmaxcriteria = minmaxcriteria)
+  Electre_4_paramCheck(PM = PM, P = P, Q = Q, V = V,
+                       minmaxcriteria = minmaxcriteria)
 
   PM <- util_pm_minmax(PM, minmaxcriteria) #validate minmax and invert scales if necessary
   ncri  <- ncol(PM)  #no. of criteria
@@ -17,53 +21,63 @@ Electre_4 <- function(PM, P, Q, V, minmaxcriteria = 'max', VERBOSE = FALSE){
   alt  <- rownames(PM) #names of alternatives
 
   #count outranking relations
-  d  <- matrix(data = 0, nrow = nalt, ncol = nalt)  # template zero matrix of n x n, n = no. of alternatives
+  # template zero matrix of n x n, n = no. of alternatives
+  d  <- matrix(data = 0, nrow = nalt, ncol = nalt)
   colnames(d) <- alt
   rownames(d) <- alt
   mp <- d #bFa: number of criteria for which option b is strictly preferred to a
   mq <- d #bSa: number of criteria for which option b is weakly preferred to a
   mi <- d #bIa: number of criteria for which option b is judged indifferent to a
-  mo <- d #mo(b,a) = mo(a,b) : the number of criteria on which options a and b perform identically
+  mo <- d #mo(b,a) = mo(a,b) : the number of criteria on which options a and b
+  #        perform identically
   veto  <- d
-  for(i in 1:nalt){
-    for(j in 1:nalt){
-      if (i != j){
-        diff <- PM[j,] - PM[i,]
-        mp[i,j] <- mp[i,j] + sum(diff > P)
-        mq[i,j] <- mq[i,j] + sum(diff > Q & diff <= P)
-        mi[i,j] <- mi[i,j] + sum(diff >= -Q & diff <= Q & diff > 0)
-        mo[i,j] <- mo[i,j] + sum(diff == 0)
-        veto[i,j] <- veto[i,j] + sum(diff >= V)
+  for (i in 1:nalt) {
+    for (j in 1:nalt) {
+      if (i != j) {
+        diff <- PM[j, ] - PM[i, ]
+        mp[i, j] <- mp[i, j] + sum(diff > P)
+        mq[i, j] <- mq[i, j] + sum(diff > Q & diff <= P)
+        mi[i, j] <- mi[i, j] + sum(diff >= -Q & diff <= Q & diff > 0)
+        mo[i, j] <- mo[i, j] + sum(diff == 0)
+        veto[i, j] <- veto[i, j] + sum(diff >= V)
       }
     }
   }
   #credibility matrix
   cred_matrix <- d
-  for(i in 1:nalt){
-    for(j in 1:nalt){
-      if (i != j){
-        diff_mp <- mp[i,j]
-        diff_mq <- mq[i,j]
-        diff_mi <- mi[i,j]
-        diff_ji <- mi[j,i] + mq[j,i] + mp[j,i]
-        if (diff_mp + diff_mq == 0 && diff_mi < diff_ji){
-          #Quasi-dominance Sq: b outranks a with quasi-dominance if b Sq a <=> mp(a,b) + mq(a,b) = 0 and mi(a,b) < mi(b,a) + mq(b,a) + mp(b,a)
-          cred_matrix[i,j] = 1.0
-        } else if(diff_mp == 0 && diff_mq <= mq[j,i] && diff_mq + diff_mi <= diff_ji + 1) {
-          # Canonical dominance Sc: b outranks a with canonical dominance if b Sc a <=> mp(a,b) = 0, and mq(a,b) <= mq(b,a), and mq(a,b) + mi(a,b) <= mi(b,a) + mq(b,a) + mp(b,a) + 1
-          cred_matrix[i,j] = 0.8
-        } else if (diff_mp == 0 && diff_mq <= mq[j,i] + mp[j,i]) {
-          # Pseudo-dominance Sp: b outranks a with pseudo-dominance if b Sp a <=> mp(a,b) = 0, and II1q(a,b) S; II1q(b,a) + mp(b,a)
-          cred_matrix[i,j] = 0.6
+  for (i in 1:nalt) {
+    for (j in 1:nalt) {
+      if (i != j) {
+        diff_mp <- mp[i, j]
+        diff_mq <- mq[i, j]
+        diff_mi <- mi[i, j]
+        diff_ji <- mi[j, i] + mq[j, i] + mp[j, i]
+        if (diff_mp + diff_mq == 0 && diff_mi < diff_ji) {
+          #Quasi-dominance Sq: b outranks a with quasi-dominance:
+          # if b Sq a <=> mp(a,b) + mq(a,b) = 0 and mi(a,b) < mi(b,a) +
+          # cont. prev. row: + mq(b,a) + mp(b,a)
+          cred_matrix[i, j] <- 1.0
+        } else if (diff_mp == 0 && diff_mq <= mq[j, i] &&
+                     diff_mq + diff_mi <= diff_ji + 1) {
+          # Canonical dominance Sc: b outranks a with canonical dominance
+          # if b Sc a <=> mp(a,b) = 0, and mq(a,b) <= mq(b,a),
+          # and mq(a,b) + mi(a,b) <= mi(b,a) + mq(b,a) + mp(b,a) + 1
+          cred_matrix[i, j] <- 0.8
+        } else if (diff_mp == 0 && diff_mq <= mq[j, i] + mp[j, i]) {
+          # Pseudo-dominance Sp: b outranks a with pseudo-dominance
+          # if b Sp a <=> mp(a,b) = 0, and II1q(a,b) S; II1q(b,a) + mp(b,a)
+          cred_matrix[i, j] <- 0.6
         } else if (diff_mp == 0) {
-          cred_matrix[i,j] = 0.4
-        } else if(diff_mp == 1 && mp[j,i] >= ncri/2) {
-          # Veto dominance Sv: b outranks a with veto-dominance if mp(a,b) = 1, b Sv a: mp(b,a) >= m/2, and gj(b) + vj[gj(b)] >= gj(a)
-          le <- all(PM[j,] + V >= PM[i,])
-          if(le) cred_matrix[i,j] = 0.2
-        } else if(diff_mp == 0){
+          cred_matrix[i, j] <- 0.4
+        } else if (diff_mp == 1 && mp[j, i] >= ncri / 2) {
+          # Veto dominance Sv: b outranks a with veto-dominance
+          # if mp(a,b) = 1, b Sv a: mp(b,a) >= m/2,
+          # and gj(b) + vj[gj(b)] >= gj(a)
+          le <- all(PM[j, ] + V >= PM[i, ])
+          if (le) cred_matrix[i, j] <- 0.2
+        } else if (diff_mp == 0) {
           # Veto dominance Sv: b outranks a with veto-dominance if mp(a,b) = 0
-          cred_matrix[i,j] = 0.2
+          cred_matrix[i, j] <- 0.2
         }
       }
     }
@@ -77,33 +91,34 @@ Electre_4 <- function(PM, P, Q, V, minmaxcriteria = 'max', VERBOSE = FALSE){
   t_fr <- finalRanking(alt, rank_P)
   final_ranking <- t_fr$final_ranking
   #construct adjancancy matrix
-  names_matrank_adj=final_ranking[,1]
+  names_matrank_adj <- final_ranking[, 1]
   matrank_adj <- matrix(data = 0, nrow = nalt, ncol = nalt)
-  rownames(matrank_adj)=names_matrank_adj
-  colnames(matrank_adj)=names_matrank_adj
-  PIR <- c('P+', 'I', 'R')
-  for (i in 1:nalt){
-    for (j in 1:nalt){
-      if(final_ranking[j,3]-final_ranking[i,3] == 1 && rank_P[final_ranking[i,1],final_ranking[j,1]] %in% PIR){
-        matrank_adj[i,j] = 1
+  rownames(matrank_adj) <- names_matrank_adj
+  colnames(matrank_adj) <- names_matrank_adj
+  PIR <- c("P+", "I", "R")
+  for (i in 1:nalt) {
+    for (j in 1:nalt) {
+      if (final_ranking[j, 3] - final_ranking[i, 3] == 1 &&
+            rank_P[final_ranking[i, 1], final_ranking[j, 1]] %in% PIR) {
+        matrank_adj[i, j] <- 1
       }
     }
   }
 
-  if(VERBOSE){
-    print('Credibility matrix')
+  if (VERBOSE) {
+    print("Credibility matrix")
     print(cred_matrix)
-    print('First pre-order (down)')
+    print("First pre-order (down)")
     print(rank_D)
-    print('First pre-order (up)')
+    print("First pre-order (up)")
     print(rank_A)
-    print('total')
+    print("total")
     print(rank_P)
-    print('Adjancancy matrix')
+    print("Adjancancy matrix")
     print(matrank_adj)
-    print('Final ranking')
+    print("Final ranking")
     print(final_ranking)
-    print('Unsorted final ranking')
+    print("Unsorted final ranking")
     print(t_fr$finalRankingUnsorted)
   }
 
--- MCDASupport/R/Electre_4_sensitivity.R
+++ MCDASupport/R/Electre_4_sensitivity.R
@@ -1,14 +1,18 @@
+# Test sensitivity for ELECTRE IV method parameters
+#
 # parameters
 #   PM - performance matrix criteria in columns and alternatives in rows
 #   P  - preference threshold
 #   Q  - indifference threshold
 #   V  - veto threshold
 #
-# solution for the problem is dominance matrix which can be visualized into network graph
-Electre_4_sensitivity <- function(PM, P, Q, V, minmaxcriteria = 'max'){
+# solution for the problem is dominance matrix which can be visualized
+# into network graph
+Electre_4_sensitivity <- function(PM, P, Q, V, minmaxcriteria = "max") {
 
   # check validity of the objects manipulated by the current function
-  Electre_4_paramCheck(PM = PM, P = P, Q = Q, V = V, minmaxcriteria = minmaxcriteria)
+  Electre_4_paramCheck(PM = PM, P = P, Q = Q, V = V,
+                       minmaxcriteria = minmaxcriteria)
   ncri <- length(P)
   nalt <- nrow(PM)
   cri <- colnames(PM)
@@ -24,18 +28,18 @@ Electre_4_sensitivity <- function(PM, P, Q, V, minmaxcriteria = 'max'){
   hyp_P <- hyp_Q <- hyp_V <- matrix(0, nrow = 21, ncol = ncri)
   colnames(hyp_P) <- colnames(hyp_Q) <- colnames(hyp_V) <- cri
   nhyp <- 21 # value of thresholds +/- 10 % should always be 21 elements
-  for(i in 1:ncri){ # for all criteria
+  for (i in 1:ncri) { # for all criteria
     t_p <- tenPercent(P[i])
     t_q <- tenPercent(Q[i])
     t_v <- tenPercent(V[i])
-    hyp_P[,i] <- t_p
-    hyp_Q[,i] <- t_q
-    hyp_V[,i] <- t_v
+    hyp_P[, i] <- t_p
+    hyp_Q[, i] <- t_q
+    hyp_V[, i] <- t_v
 
     # sensitivity of the preference thresholds
     hyp_P_result <- hyp_Q_result <- hyp_V_result <- matrix(0, ncol = nalt, nrow = 21)
     colnames(hyp_P_result) <- colnames(hyp_Q_result) <- colnames(hyp_V_result) <- alt
-    for(j in 1:nhyp){ #iterate over values t_* for the criterium
+    for (j in 1:nhyp) { #iterate over values t_* for the criterium
       thres <- P
       thres[i] <- t_p[j]
       tp <- Electre_4(PM, thres, Q, V, minmaxcriteria, FALSE)
@@ -45,9 +49,9 @@ Electre_4_sensitivity <- function(PM, P, Q, V, minmaxcriteria = 'max'){
       thres <- V
       thres[i] <- t_v[j]
       tv <- Electre_4(PM, P, Q, thres, minmaxcriteria, FALSE)
-      hyp_P_result[j,] <- tp$finalRankingUnsorted
-      hyp_Q_result[j,] <- tq$finalRankingUnsorted
-      hyp_V_result[j,] <- tv$finalRankingUnsorted
+      hyp_P_result[j, ] <- tp$finalRankingUnsorted
+      hyp_Q_result[j, ] <- tq$finalRankingUnsorted
+      hyp_V_result[j, ] <- tv$finalRankingUnsorted
     }
 
     hyp_P_cri[[i]] <- hyp_P_result
@@ -58,11 +62,11 @@ Electre_4_sensitivity <- function(PM, P, Q, V, minmaxcriteria = 'max'){
     hyp_Q_result <- as.data.frame(hyp_Q_result)
     hyp_V_result <- as.data.frame(hyp_V_result)
 
-    title <- paste0('preference threshold for ', cri[i])
+    title <- paste0("preference threshold for ", cri[i])
     hyp_P_cri_graph[[i]] <- rankGraph(hyp_P_result, t_p, title)
-    title <- paste0('indifference threshold for ', cri[i])
+    title <- paste0("indifference threshold for ", cri[i])
     hyp_Q_cri_graph[[i]] <- rankGraph(hyp_Q_result, t_q, title)
-    title <- paste0('veto threshold for ', cri[i])
+    title <- paste0("veto threshold for ", cri[i])
     hyp_Q_cri_graph[[i]] <- rankGraph(hyp_Q_result, t_p, title)
   }
 
