9fe1c2274bc7355bc1f1dabef6f7091ccd471bc5
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-05-03 10:00:48 +0200

    optimization of util_* functions git push

--- MCDASupport/R/util_pm_minmax.R
+++ MCDASupport/R/util_pm_minmax.R
@@ -1,12 +1,13 @@
-util_pm_minmax <- function(PM, minmaxcriteria = 'max') {
+util_pm_minmax <- function(PM, minmaxcriteria = "max") {
   minmax <- util_prepare_minmaxcriteria(ncol(PM), minmaxcriteria)
-  if (length(minmax[minmax %in% c("min")]) > 0){  #some criteria are minimalized - need to invert them
+  if (length(minmax[minmax %in% c("min")]) > 0) {
+    # some criteria are minimalized - need to invert them
     i <- 0
-    for(r in minmax){
+    for (r in minmax) {
       i <- i + 1
-      if(r == 'min'){ #invert scale
-        valmax <- max(PM[,i])
-        PM[,i] <- valmax - PM[,i]
+      if (r == "min") { #invert scale
+        valmax <- max(PM[, i])
+        PM[, i] <- valmax - PM[, i]
       }
     }
   }
