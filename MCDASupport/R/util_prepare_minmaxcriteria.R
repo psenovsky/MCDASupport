70f3b8eefb8ec80374fc523004bbbff618f4dedf
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-05-03 11:01:27 +0200

    corrected error in usage of any function in util_prepeare_minmax.

--- MCDASupport/R/util_prepare_minmaxcriteria.R
+++ MCDASupport/R/util_prepare_minmaxcriteria.R
@@ -1,11 +1,11 @@
 util_prepare_minmaxcriteria <- function(ncrit, minmaxcriteria = "max") {
   # validate parameters
-  if (!is.vector(minmaxcriteria) || length(minmaxcriteria) == 1) {
+  if (length(minmaxcriteria) == 1) {
     if (!is.numeric(ncrit)) stop("expected number of criteria to be a number")
     if (ncrit < 2) stop("at minimum 2 criteria need to be considered")
   }
   minmax <- tolower(minmaxcriteria)
-  if (any(minmax != c("min", "max"))) {
+  if (any(!(minmax %in% c("min", "max")))) {
     stop("Vector minmaxcriteria must contain max or min")
   }
   if (is.vector(minmaxcriteria) && length(minmaxcriteria) > 1) {
