75b253641f92158ab4a97b8b69db807af30dbd0f
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-06-21 11:04:47 +0200

    clean up of Electre_1 function.

--- MCDASupport/R/Electre_1.R
+++ MCDASupport/R/Electre_1.R
@@ -3,44 +3,58 @@
 #   w  - weights
 #   minmaxcriteria - vector of min/max for the criteria to se direction of the
 #                    optimisation
-#   concordance_threshold = 1
-#   discordance_threshold = 0
-#   VERBOSE = FALSE
+#   concordance_threshold 1
+#   discordance_threshold 0
+#   VERBOSE FALSE
+#   test    TRUE to use build in parameter tests, use FASLSE when doing
+#           sensitivity testing
 # solution for the problem is dominance matrix which can be
 # visualized into network graph
 Electre_1 <- function(PM, w,
                       minmaxcriteria = "max",
                       concordance_threshold = 1,
                       discordance_threshold = 0,
-                      VERBOSE = FALSE) {
+                      VERBOSE = FALSE,
+                      test = TRUE) {
 
   ## check validity of the objects manipulated by the current function
   # if there < 2 criteria or alternatives, there is no MCDA problem
-  if (is.null(dim(PM))) stop("less than 2 criteria or 2 alternatives")
-  if (!(is.matrix(PM) || (is.data.frame(PM)))) {
-    stop("wrong performance matrix, should be a matrix or a data frame")
+  test_parameters <- function(pm, w, minmaxcriteria, c_threshold, d_threshold) {
+    if (!(is.matrix(pm) || (is.data.frame(pm)))) {
+      stop("wrong performance matrix, should be a matrix or a data frame")
+    }
+    if (!is.numeric(unlist(pm))) {
+      stop("Only numeric values in performance matrix expected")
+    }
+    if (!(is.vector(w, mode = "numeric"))) {
+      stop("criteriaWeights should be a numeric vector")
+    }
+    if (ncol(pm) != length(w)) {
+      stop("length of criteriaWeights should be checked")
+    }
+    if (!is.numeric(c_threshold) || c_threshold < 0 || c_threshold > 1) {
+      stop("Concordance threshold out of bounds")
+    }
+    if (!is.numeric(d_threshold) || d_threshold < 0 || d_threshold > 1) {
+      stop("Discordance threshold out of bounds")
+    }
   }
-  if (!is.numeric(unlist(PM))) {
-    stop("Only numeric values in performance matrix expected")
-  }
-  if (!(is.vector(w, mode = "numeric"))) {
-    stop("criteriaWeights should be a numeric vector")
-  }
-  if (ncol(PM) != length(w)) stop("length of criteriaWeights should be checked")
-  PM <- util_pm_minmax(PM, minmaxcriteria) #validate minmax and invert scales if necessary
-  if (!is.numeric(concordance_threshold) || concordance_threshold < 0 || concordance_threshold > 1) stop('Concordance threshold out of bounds')
-  if (!is.numeric(discordance_threshold) || discordance_threshold < 0 || discordance_threshold > 1) stop('Discordance threshold out of bounds')
+  if (test) test_parameters(PM, w, minmaxcriteria, concordance_threshold,
+                            discordance_threshold)
+  #validate minmax and invert scales if necessary
+  pm <- util_pm_minmax(PM, minmaxcriteria)
   ## End of checking the validity of the "inputs"
 
-  nalt <- nrow(PM)  #no. of alternatives
-  alt  <- rownames(PM)
-  cm <- ELECTRE_ConcordanceMatrix(PM, w)  #Concordance matrix
-  dm <- ELECTRE_DiscordanceMatrix(PM) #discordance matrix
+  nalt <- nrow(pm)  #no. of alternatives
+  alt  <- rownames(pm)
+  cm <- ELECTRE_ConcordanceMatrix(pm, w)  #Concordance matrix
+  dm <- ELECTRE_DiscordanceMatrix(pm) #discordance matrix
 
   #dominance matrix (preference matrix)
   dom <- matrix(data = 0, nrow = nalt, ncol = nalt)
   #aSb iff C(a,b) >= c_thres and D(a,b) <= d_thres
-  dom <- ifelse((cm >= concordance_threshold & dm <= discordance_threshold), 1, dom)
+  dom <- ifelse((cm >= concordance_threshold & dm <= discordance_threshold),
+                1, dom)
   diag(dom) <- 0
   rownames(dom) <- alt
   colnames(dom) <- alt
@@ -61,7 +75,7 @@ Electre_1 <- function(PM, w,
   }
 
   #t(cm) - pyDecisions provide diagonaly mirrored matrixes (also for dm)
-  out <- list(PerformanceMatrix = PM,
+  out <- list(PerformanceMatrix = pm,
               ConcordanceMatrix = t(cm),
               DiscordanceMatrix = t(dm),
               PreferenceMatrix = dom,
