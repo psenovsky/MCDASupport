18e24d8447fbde2d62abb6f0397374ed13471d39
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-09-04 10:42:11 +0200

    another error in keywords generation (prevented to build the manual)

--- MCDASupport/R/EWM.R
+++ MCDASupport/R/EWM.R
@@ -53,11 +53,11 @@
 #' @return
 #' list:
 #' \itemize{
-#'   \item normalizedPM - normalized performance matrix (with all criteria
-#'  transformed to maximalize direction)
-#'   \item probabilityCriteria - probability of criteria to occur
-#'   \item degreeDivergence - degree of divergence
-#'   \item entropyWeight - weights derived by EWM method for the criteria
+#'   \item{normalizedPM}{normalized performance matrix (with all criteria
+#'  transformed to maximalize direction)}
+#'   \item{probabilityCriteria}{probability of criteria to occur}
+#'   \item{degreeDivergence}{degree of divergence}
+#'   \item{entropyWeight}{weights derived by EWM method for the criteria}
 #' }
 #'
 #' @references
@@ -72,21 +72,19 @@
 #'   c(103000,101300,156400,267400,49900,103600,103000,170100,279700,405000),
 #'   c(171.3,205.3,221.7,230.7,122.6,205.1,178.0,226.0,233.8,265.0),
 #'   c(7.65,7.90,7.90,10.50,8.30,8.20,7.20,9.10,10.90,10.30),
-#'   c(352,203,391,419,120,265,419,419,359,265),
+#'   c(352,203,391,419,120,265,419,419,359,265),
 #'   c(11.6,8.4,8.4,8.6,23.7,8.1,11.4,8.1,7.8,6.0),
 #'   c(88.0,78.3,81.5,64.7,74.1,81.7,77.6,74.7,75.5,74.7),
 #'   c(69.7,73.4,69.0,65.6,76.4,73.6,66.2,71.7,70.9,72.0))
 #' rownames(PM) <- c("CBX16","P205G","P405M","P605S","R4GTL",
-#'   "RCLIO","R21TS","R21TU","R25BA","ALPIN")
+#'   "RCLIO","R21TS","R21TU","R25BA","ALPIN")
 #' colnames(PM) <- c("Prix","Vmax","C120","Coff","Acce","Frei","Brui")
 #' minmaxcriteria <-c("min","max","min","max","min","min","min")
 #' M <- EWM(PM, minmaxcriteria)
 #'
 #' @author Pavel Šenovský \email{pavel.senovsky@vsb.cz}
 #'
-#' @keywords EWM
-#' @keywords Entropy Weight Method
-#' @keywords weight
+#' @keywords EWM Entropy Weight Method
 EWM <- function(PM, minmaxcriteria = "max", VERBOSE = FALSE) {
 
   ## check validity of the objects manipulated by the current function
