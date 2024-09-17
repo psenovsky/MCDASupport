d00ed040bb57bccae760f9aab91b76b80a72f344
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-09-17 09:21:29 +0200

    added roxygen documentation

--- MCDASupport/R/topsis_ideal.R
+++ MCDASupport/R/topsis_ideal.R
@@ -1,12 +1,39 @@
-# topsis_ideal - function for computation of ideal and antiideal solution
-# this function is being utilized by TOPSIS and SIR functions.
-#
-# function is intended for internal usage only.
-#
-# parameters:
-#   pm - normalized performance matrix
-#   inferiority - compute inferiority index with a_ideal and a_anti switched,
-#                 FALSE by default
+#' function to compute ideal and antiedeal solution for TOPSIS and SIR
+#'  (internal use only)
+#'
+#' @description
+#' Function to compute ideal and antiideal solution to normalized perfomance
+#'  matrix.
+#'
+#' The function is utilized by \code{\link{SIR}} and \link{TOPSIS} methods for
+#'  this purpose. It incorporates steps 3 - 5 of the TOPSIS computation.
+#'  Consult \link{TOPSIS} function's documentation for implementation details.
+#'
+#' @param pm normalized performance matrix
+#' @param inferiority FALSE by defaul to compute ideals as max from pm, TRUE
+#'  will minimize ideal and maximize anitiideal
+#'
+#' @return
+#' Returns list with following structure:
+#' \itemize{
+#'   \item a_ideal - Positive ideal soluation A*
+#'   \item a_anti - Anti-ideal solution A-
+#'   \item d_ideal - Closenes to ideal variant
+#'   \item d_anti - Closeness to anti-ideal variant
+#'   \item closenes - Relative closeness of the alternatives to ideal solution
+#' }
+#'
+#' @references
+#' ALAOUI, Mohamed El. Fuzzy TOPSIS: Logic, Approaches, and Case Studies. Boca
+#'  Raton: CRC Press, 2021. 216 s. ISBN 978-0-367-76748-8.
+#'
+#' Papathanasiou, Jason, Ploskas, Nikolaos. Multiple Criteria Decision Aid
+#'  Methods, Examples and Python Implementations. Springer, 173 p., ISBN
+#'  978-3-319-91648-4.
+#'
+#' @author Pavel Šenovský \email{pavel.senovsky@vsb.cz}
+#'
+#' @keywords TOSPIS SIR ideal anti-ideal
 topsis_ideal <- function(pm, inferiority = FALSE) {
   ncri <- ncol(pm)     # no. of criteria
   nalt <- nrow(pm)     # no. of alternatives
