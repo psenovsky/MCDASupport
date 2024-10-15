17178e2af675c684b558376313e5ddc9ec33ad5f
Pavel Šenovský <pavel.senovsky@vsb.cz>
2024-10-15 10:39:33 +0200

    now uses validation infrastructure

--- MCDASupport/R/mcda_rescale_pm.R
+++ MCDASupport/R/mcda_rescale_pm.R
@@ -39,12 +39,7 @@
 #' @keywords MCDA scaling performance matrix
 mcda_rescale_pm <- function(m) {
   #parameter validity check
-  if (!(is.matrix(m) || (is.data.frame(m)))) {
-    stop("wrong performance matrix, should be a matrix or a data frame")
-  }
-  if (ncol(m) < 2 || nrow(m) < 2) stop("less than 2 criteria or 2 alternatives")
-  i <- sapply(m, is.numeric)
-  if (length(i[i %in% FALSE]) > 0) stop("Performance matrix must be numeric")
+  validation$validate_pm(m)
 
   col_maxs <- function(m) apply(m, 2, max, na.rm = TRUE)
   col_mins <- function(m) apply(m, 2, min, na.rm = TRUE)
